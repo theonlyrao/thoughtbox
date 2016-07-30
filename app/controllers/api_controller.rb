91e3c4e0ecf775b260e3bc175af24e635da4a058
Ashwin Rao <theonlyrao@gmail.com>
2016-07-30 14:05:21 -0600

    Scope links to current_user

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,9 @@
 class ApiController < ActionController::Base
   respond_to :json
+  helper_method :current_user  
+
+  def current_user
+    @current_user ||= User.find(session[:user]) if session[:user]    
+  end
 end
 
