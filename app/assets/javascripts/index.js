89a0579814a9ff34c3f12fe919496c4a31cf581d
Ashwin Rao <theonlyrao@gmail.com>
2016-07-30 12:12:05 -0600

    Implement alphabetical sorting

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,21 @@
 $(document).ready(function(){
     $("#alphabetize").click(function(){
-	console.log("alphabetize")
+	var $link = $(".link").toArray();
+
+	var mapped = $link.map(function(link, i) {
+	    return { index: i, value: link.dataset.title };
+	})
+
+	mapped.sort(function(a, b) {
+	    return +(a.value > b.value) || +(a.value === b.value) - 1;
+	});
+
+	var alphabetical = mapped.map(function(el){
+	    return $link[el.index];
+	});
+
+	$(".link").remove();
+	$(".link-list").append(alphabetical);
     })
     
     $('input[type=radio][name=read]').change(function() {
@@ -51,9 +66,9 @@ $(document).ready(function(){
     
     var displayLink = function(link){
 	if(link.read === true){
-	    $(".link-list").append("<div class='link link-read' ><div id="+link.title+">" + link.title + "</div> " + link.address + "<br /><button type='button' class='button-read' data-id=" + link.id + ">Mark as Unread</button><a href=" + "/links/" + link.id + " type='button' class='button-edit' data-id=" + link.id + ">Edit</a></div>")
+	    $(".link-list").append("<div class='link link-read' data-title="+link.title+"><div>" + link.title + "</div> " + link.address + "<br /><button type='button' class='button-read' data-id=" + link.id + ">Mark as Unread</button><a href=" + "/links/" + link.id + " type='button' class='button-edit' data-id=" + link.id + ">Edit</a></div>")
 	} else {
-	    $(".link-list").append("<div class='link link-unread'><div id="+link.title+">" + link.title + "</div> " + link.address + "<br /><button type='button' class='button-unread' data-id=" + link.id + ">Mark as Read</button><a href=" + "/links/" + link.id + " type='button' class='button-edit' data-id=" + link.id + ">Edit</a></div>")
+	    $(".link-list").append("<div class='link link-unread' data-title="+link.title+"><div>" + link.title + "</div> " + link.address + "<br /><button type='button' class='button-unread' data-id=" + link.id + ">Mark as Read</button><a href=" + "/links/" + link.id + " type='button' class='button-edit' data-id=" + link.id + ">Edit</a></div>")
 	}
     };
 
@@ -86,7 +101,7 @@ $(document).ready(function(){
 	var address = $("#address").val();
 
 	
-	if(( address.substring(0, 11) === "http://www." || address.substring(0, 12) === "https://www." ) && address.substring(address.length - 4 ) === ".com"){
+	if(( address.substring(0, 11) === "http://www." || address.substring(0, 12) === "https://www." ) && (address.substring(address.length - 4 ) === ".com" || address.substring(address.length - 3) === ".io")){
 	    $.ajax({
 		type: "POST",
 		url: "/api/v1/links",
